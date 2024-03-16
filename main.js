85d7465f851a901edcf7afe161e7c045394d6a5c
Aqsa Naseer <arzoosultani123456@gmail.com>
2024-03-17 00:06:29 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,22 @@
+"use strict";
+// Task#6; Stripping Names: Store a person’s name, 
+//and include some whitespace characters at the beginning and end of the name.
+// Use "\t" (tab) and "\n" (new line) at least once. Print the name once, 
+//so the whitespace around the name is displayed. Then print the name after stripping the white spaces.
+let Name = "\t\n AQSA Naseer \t\n";
+console.log(Name);
+console.log(Name.trim());
+// Task#7 ; Number Eight: Write addition, subtraction, multiplication,
+// and division operations that each result in the number 8. 
+//Enclose your operations in print statements.
+console.log(10 - 2);
+console.log(4 + 4);
+console.log(4 * 2);
+console.log(16 / 2);
+// task#8: You should create four lines that look like this:
+//console.log(5 + 3)
+//Your output should simply be four lines with the number 8 appearing once on each line.
+console.log(7 + 1);
+console.log(10 - 2);
+console.log(2 * 4);
+console.log(24 / 3);
